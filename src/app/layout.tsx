3e88ba81374679706d767be19882b20c505f17ba
Caio Oliveira <xcaio2@gmail.com>
2026-08-12 09:37:44 +0100

    added meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,9 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  verification: {
+    google: "bgcY2uX3QJ1LJinpok5ZaxnNN7uRaoOqicMUTxD5IWc",
+  },
   other: {
     "geo.region": "BR-RJ",
     "geo.placename": "Niterói",
